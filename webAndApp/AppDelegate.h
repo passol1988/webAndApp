641da48a63db4e8576a1d265603b9ba592d47ff5
赵宇 <18642802746@163.com>
2016-01-18 20:53:49 +0800

    Initial Commit

---
+++ webAndApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  webAndApp
+//
+//  Created by 赵宇 on 16/1/18.
+//  Copyright © 2016年 赵宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ webAndApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  webAndApp
+//
+//  Created by 赵宇 on 16/1/18.
+//  Copyright © 2016年 赵宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
